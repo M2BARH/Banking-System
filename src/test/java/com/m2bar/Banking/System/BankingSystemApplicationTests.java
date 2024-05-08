c5645ce2f7f6b5762d79d09343a2cee61cfb8933
m2barh <bongani@protogen.co.za>
2024-05-08 19:49:16 +0200

    Initial commit

---
+++ src/test/java/com/m2bar/Banking/System/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2bar.Banking.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
